75f5457aecf54de9add02a5e1e7a438734411818
Cintia Lopes <135193611+cindyyershees@users.noreply.github.com>
2023-09-27 16:38:26 -0300

    Add files via upload

--- entradadeusuarios.py
+++ entradadeusuarios.py
@@ -7,9 +7,10 @@ n2 = float (input('Digite a nota 2: '))
 #processamento
 media = (n1 + n2)/2
 #saída
-print('Nome do aluno: ', aluno.upper()) #letras maiúsculas
-print('Nome do aluno: ', aluno.lower()) #letras minúsculas
+print(f'Nome do aluno:  {aluno.upper()} - {media}')  #letras maiúsculas
+print('Nome do aluno:', aluno.lower() ,' média:' , media ) #letras minúsculas
 print('Nome do aluno: ', aluno.title()) #letras iniciais dos nomes são escritas em maíusculo
+
 
 print('Nota 1: ', n1)
 print('Nota 2: ' , n2)
